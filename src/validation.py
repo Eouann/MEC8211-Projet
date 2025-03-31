024714c91529c7346e51784d61f5c44c9beafc5d
E'ouann Kerino <57634751+Eouann@users.noreply.github.com>
2025-03-31 14:01:23 -0400

    importations

--- src/validation.py
+++ src/validation.py
@@ -2,3 +2,9 @@
 Fichier de validation du modèle numérique de diffusion thermique
 """
 
+
+# Importation des bibliothèques
+import numpy as np
+import matplotlib.pyplot as plt
+
+
--- src/verification.py
+++ src/verification.py
@@ -2,3 +2,9 @@
 Fichier de vérification du modèle numérique de diffusion thermique
 """
 
+
+# Importation des bibliothèques
+import numpy as np
+import matplotlib.pyplot as plt
+
+
