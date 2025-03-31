1667ee934c46eca607022ef8e54b1985ddefccbd
E'ouann Kerino <57634751+Eouann@users.noreply.github.com>
2025-03-31 13:54:22 -0400

    fichier de base de la simu et erreurs L1,L2,Linf

---
+++ erreurs.py
@@ -0,0 +1,28 @@
+"""
+Fichier de calcul des erreurs L1, L2 et Linf discrètes
+"""
+
+
+#Importation de bibliothèques
+import numpy as np
+
+
+#Erreur L1 discrète
+def ErreurL1(Ti,Ti_exact,N):
+    """Fonction de calcul de l'erreur L1 discrète"""
+    normeL1 = 1/N*np.sum(np.abs(Ti-Ti_exact))
+    return normeL1
+
+
+#Erreur L2 discrète
+def ErreurL2(Ti,Ti_exact,N):
+    """Fonction de calcul de l'erreur L2 discrète"""
+    normeL2 = np.sqrt(1/N*np.sum(np.abs(Ti-Ti_exact)**2))
+    return normeL2
+
+
+#Erreur Linf discrète
+def ErreurLinf(Ti,Ti_exact):
+    """Fonction de calcul de l'erreur Linf discrète"""
+    normeLinf = np.max(np.abs(Ti-Ti_exact))
+    return normeLinf
---
+++ lancer_simulation.py
@@ -0,0 +1,76 @@
+"""
+Fichier de lancement de simulation de diffusion thermique au travers d'un matériau isotrope
+"""
+
+
+# Importation des bibliothèques
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Définition des constantes
+alpha=0.3e-6        # m²/s (matière réfractaire)
+e=0.1               # m (épaisseur du mur du four)
+T_0=20+273          # K (température initiale du mur)
+T_x_0=1600+273      # K (température de la flamme du four)
+T_x_e=20+273        # K (température de l'air ambiant dans l'atelier)
+t_max=3600          # s (durée de la simulation)
+
+
+# Calcul des températures pour N points
+def Concentrations(N_spatial,N_temporel):
+    """Fonction de calcul des N températures en différences finies"""
+    T_i = np.ones(N_spatial)*T_0                # Vecteur des N températures numériques calculées T_i
+    T_i_n = np.zeros((N_temporel,N_spatial))    # Matrice des N températures numériques calculées T_i pour chaque itération
+    matA = np.zeros((N_spatial,N_spatial))      # Matrice A pour la résolution du système matriciel
+    vectB = np.zeros(N_spatial)                 # Vecteur B pour la résolution du système matriciel
+
+    delta_x=e/(N_spatial-1)                     # Pas de discrétisation
+    x_i=np.linspace(0, e, N_spatial)            # Vecteur des N points r_i également espacées
+
+    delta_t=t_max/N_temporel                    # Pas de discrétisation temporelle
+    t_i=np.linspace(0, t_max, N_temporel)       # Vecteur des N points t_i également espacées
+
+    # Condition de Dirichlet en x = 0
+    matA[0,0] = 1
+    vectB[0] = T_x_0
+
+    # Condition de Dirichlet en x = e
+    matA[-1,-1] = 1
+    vectB[-1] = T_x_e
+
+
+    # Algorithmes differences finies
+    for i in range(1,N_spatial-1):
+        matA[i,i-1] = -alpha*delta_t                # Coeff B devant T_i-1
+        matA[i,i] = delta_x**2+2*alpha*delta_t      # Coeff A devant T_i
+        matA[i,i+1] = -alpha*delta_t                # Coeff B devant T_i+1
+    
+    for i in range(len(t_i)):
+        t=t_i[i]
+        for j in range(1, N_spatial - 1):
+            vectB[j] = T_i[j]*delta_x**2
+
+        # Résolution du système matriciel
+        T_i = np.linalg.solve(matA, vectB)
+        T_i_n[i] = T_i
+
+    return T_i_n,x_i,t_i
+
+
+# Lancement de la simulation
+N_spatial=100
+N_temporel=100
+T_i_n,x_i,t_i = Concentrations(N_spatial,N_temporel)
+
+
+# Affichage des résultats
+plt.plot(x_i,T_i_n[0],label='t=0s')
+plt.plot(x_i,T_i_n[25],label='t=900s')
+plt.plot(x_i,T_i_n[50],label='t=1800s')
+plt.plot(x_i,T_i_n[75],label='t=2700s')
+plt.plot(x_i,T_i_n[99],label='t=3600s')
+plt.xlabel('Position x (m)')
+plt.ylabel('Température T (K)')
+plt.legend()
+plt.show()
